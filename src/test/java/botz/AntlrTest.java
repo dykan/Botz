3f6844e866932745c05e8a894f64f46956a67088
Gal <gal@spitfire.co.il>
2014-06-30 14:29:35 +0300

    added pojo

--- src/test/java/botz/AntlrTest.java
+++ src/test/java/botz/AntlrTest.java
@@ -2,11 +2,14 @@ package botz;
 import java.io.IOException;
 
 
+import botz.antlr.JavaBaseListener;
 import org.antlr.v4.runtime.ANTLRFileStream;
 import org.antlr.v4.runtime.BufferedTokenStream;
 import org.antlr.v4.runtime.TokenSource;
 import org.antlr.v4.runtime.TokenStream;
+import org.antlr.v4.runtime.misc.NotNull;
 import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.ParseTreeListener;
 import org.antlr.v4.runtime.tree.ParseTreeWalker;
 
 import botz.antlr.JavaLexer;
@@ -16,7 +19,7 @@ import org.junit.Test;
 public class AntlrTest {
     @Test
     public void testSimpleJava() throws Exception {
-        String fileName = "src/test/java/botz/AntlrTest.java";
+        String fileName = "src/test/java/botz/Pojo.java";
         org.antlr.v4.runtime.Lexer lexer = new JavaLexer(new ANTLRFileStream(fileName));
         TokenStream tokenStream = new BufferedTokenStream(lexer);
         JavaParser javaParser = new JavaParser(tokenStream);
@@ -26,8 +29,26 @@ public class AntlrTest {
 
         // walker
         ParseTreeWalker walker = new ParseTreeWalker();
-        MyListener listener = new MyListener();
+        ParseTreeListener listener = getJavaBaseListener();
         walker.walk(listener, tree);
+    }
+
+    private JavaBaseListener getJavaBaseListener() {
+        return new JavaBaseListener() {
+            public void enterMethodDeclaration(@NotNull JavaParser.MethodDeclarationContext ctx){
+
+                System.out.println("method name: " + ctx.Identifier().getText());
+
+                JavaParser.FormalParametersContext params = ctx.formalParameters();
+                System.out.println("params: " + params.getText());
+
+                System.out.println("body: " + ctx.methodBody().getText());
+            }
+
+            public void enterImportDeclaration(@NotNull JavaParser.ImportDeclarationContext ctx){
+                System.out.println(ctx.qualifiedName().getText());
+            }
+        };
     }
 
 }
---
+++ src/test/java/botz/Pojo.java
@@ -0,0 +1,25 @@
+package botz;
+
+/**
+ * Created by schniz on 6/30/14.
+ */
+public class Pojo {
+    private int number;
+    private String string;
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getString() {
+        return string;
+    }
+
+    public void setString(String string) {
+        this.string = string;
+    }
+}

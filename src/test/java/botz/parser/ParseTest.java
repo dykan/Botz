f4ec9cfaf7d869949113b753a2fffa22081103c4
shai dykan <shai.dorit@gmail.com>
2014-06-30 16:32:29 +0300

    botz parser and tests

--- src/test/java/botz/parser/ParseTest.java
+++ src/test/java/botz/parser/ParseTest.java
@@ -2,14 +2,18 @@ package botz.parser;
 
 import java.util.ArrayList;
 
+import org.junit.Test;
+
 import botz.cstree.MethodNode;
 
 public class ParseTest {
 	
 	String fileName = "src/test/java/botz/AntlrTest.java";
 
-  public void buildMethod(){
+	@Test
+  public void renderMethod(){
     	BotzParser parser = new BotzParser(fileName);
+    	parser.parse();
     	ArrayList<MethodNode> methods  = parser.listener.root.getMethods();
     	
     	for(MethodNode method : methods){

2eee1ce5a9dd81631871a31f3fd855346af2873a
shai dykan <shai.dorit@gmail.com>
2014-06-30 13:02:28 +0300

    method node

--- src/botz/cstree/Node.java
+++ src/botz/cstree/Node.java
@@ -1,35 +1,26 @@
 package botz.cstree;
 
-import java.util.ArrayList;
+
 
 public abstract class Node {
 	
-	private ArrayList<Node> sons = new ArrayList<Node>();
 	private Node parent;
-	
-	public Node(){	}
-	
+
 	public Node(Node parent){
 		this.parent = parent;
 	}
 	
-	public void addSon(Node node){
-		sons.add(node);
+	public Node getParent() {
+		return parent;
 	}
-	
-	public abstract String renderBeforeAll();
-	public abstract String renderAfterAll();
-	public abstract String renderBeforeSon(Node son);
-	public abstract String renderAfterSon(Node son);
 
-	public void render(){
-		renderBeforeAll();
-		for (Node son : sons){
-			renderBeforeSon(son);
-			son.render();
-			renderAfterSon(son);
-		}
-		renderAfterAll();
+	public void setParent(Node parent) {
+		this.parent = parent;
 	}
+
+	public Node(){	}
+
+
+	public abstract void render(StringBuilder stb);
 
 }

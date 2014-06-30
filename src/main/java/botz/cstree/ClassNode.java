6b8a683babfe1082d70f9e42b36e13c9ac728ec2
Gal <gal@spitfire.co.il>
2014-06-30 18:10:02 +0300

    Added the classNode and the indent method

--- src/main/java/botz/cstree/ClassNode.java
+++ src/main/java/botz/cstree/ClassNode.java
@@ -2,20 +2,18 @@ package botz.cstree;
 
 import java.util.ArrayList;
 
-public class ClassNode extends Node{
-
+public class ClassNode extends Node {
+    private String name;
 	private ArrayList<MethodNode> methods; 
 	private String parentClass;
 	private ArrayList<String> interfaces;
 	private ArrayList<ParameterNode> privateMembers;
-	
-	
-	
-	
-	public ClassNode(Node parent, ArrayList<MethodNode> methods,
+
+	public ClassNode(String name, Node parent, ArrayList<MethodNode> methods,
 			String parentClass, ArrayList<String> interfaces,
 			ArrayList<ParameterNode> privateMembers) {
 		super(parent);
+        this.name = name;
 		this.methods = methods;
 		this.parentClass = parentClass;
 		this.interfaces = interfaces;
@@ -29,8 +27,31 @@ public class ClassNode extends Node{
 
 	@Override
 	public String render() {
-		// TODO Auto-generated method stub
-		return null;
+        StringBuilder classBuilder = new StringBuilder();
+        classBuilder.append(getDeclerationString()).append("\n");
+        classBuilder.append(getMethodsDeclerations()).append("\n");
+
+        return classBuilder.toString();
 	}
 
+    private String getMethodsDeclerations() {
+        StringBuilder strb = new StringBuilder();
+
+        for (MethodNode method : methods) {
+            strb.append(method.render()).append("\n\n");
+        }
+
+        return strb.toString();
+    }
+
+    private String getDeclerationString() {
+        StringBuilder lineBuilder = new StringBuilder();
+        lineBuilder.append("class ").append(name);
+
+        if (parentClass != null) {
+            lineBuilder.append(" extends ").append(parentClass);
+        }
+
+        return this.indent(lineBuilder.toString());
+    }
 }
